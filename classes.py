8899c0da87005980d14d776cbae7ac123fcbf47c
Antonioarf <49568529+Antonioarf@users.noreply.github.com>
2019-07-14 21:15:50 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -84,20 +84,19 @@ class Player(pygame.sprite.Sprite):
 
 
 
+
 class Tiro(pygame.sprite.Sprite):
     
     # Construtor da classe.
-    def __init__(self, uni_anim):
-        
-        x =  1000
-        y = random.randint(475,525) 
+    def __init__(self, tiro_img):
+        x = 1000
+        y = 600
         
         # Construtor da classe pai (Sprite).
         pygame.sprite.Sprite.__init__(self)
         
-        self.images = uni_anim
-        self.currentimg = 0
-        self.image = pygame.transform.scale(self.images[self.currentimg], (50 , 70))
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(tiro_img, (60, 30))
         
 #        # Deixando transparente.
 #        self.image.set_colorkey(BLACK)
@@ -110,19 +109,13 @@ class Tiro(pygame.sprite.Sprite):
         # Sorteia um lugar inicial em y
         self.rect.bottom = y
         # Sorteia uma velocidade inicial
-        self.speedx = -4
+        self.speedx = -6
         self.speedy = 0
         
         # Melhora a colisÃ£o estabelecendo um raio de um circulo
-        self.radius = int(self.rect.width * .65 / 2)
-    
-        # Guarda o tick da primeira imagem
-        self.last_update = pygame.time.get_ticks()
-
-        # Controle de ticks de animaÃ§Ã£o: troca de imagem a cada self.frame_ticks milissegundos.
-        self.frame_ticks = 75
+        self.radius = int(self.rect.width * .85 / 2)
         
-    # Metodo que atualiza a posio do meteoro
+    # Metodo que atualiza a posiÃ§Ã£o do meteoro
     def update(self):
         
         if self.rect.left <= 0:
@@ -131,25 +124,14 @@ class Tiro(pygame.sprite.Sprite):
             self.rect.x += self.speedx
             self.rect.y += self.speedy
             
-        now = pygame.time.get_ticks()
 
-        # Verifica quantos ticks se passaram desde a ultima mudanÃ§a de frame.
-        elapsed_ticks = now - self.last_update
 
-        # Se jÃ¡ estÃ¡ na hora de mudar de imagem...
-        if elapsed_ticks > self.frame_ticks:
 
-            # Marca o tick da nova imagem.
-            self.last_update = now
 
-            # AvanÃ§a um quadro.
-            self.currentimg += 1
 
-            # Verifica se jÃ¡ chegou no final da animaÃ§Ã£o.
-            if self.currentimg == len(self.images):
-                # Se sim, tchau explosÃ£o!
-                self.currentimg=0
-            self.image = self.images[self.currentimg]
+            
+
+
 
 
         
@@ -203,15 +185,25 @@ class Back(pygame.sprite.Sprite):
 # Carrega todos os assets uma vez sÃ³.
 def load_assets(img_dir, snd_dir, fnt_dir):
     assets = {}
-    assets["tiro"] = pygame.image.load(path.join(img_dir, "bala.png")).convert()
     assets["game_over"] = pygame.image.load(path.join(img_dir, "gameover1.png")).convert()
-    assets["lives_img"] = pygame.image.load(path.join(img_dir, "coracao.png")).convert()
     assets ["background_init"] = pygame.image.load(path.join(img_dir, 'imagem 1.jpeg')).convert()
     assets["background"] = pygame.image.load(path.join(img_dir, 'imagem de fundo_ 1.jpg')).convert()
-    assets["musica_fim"] = pygame.mixer.Sound(path.join(snd_dir, 'Game Over Sound Effects High Quality-[AudioTrimmer.com].ogg'))
+    assets["parado"] = pygame.image.load(path.join(img_dir, 'boneco_1.png')).convert()
+    assets["canhao"] = pygame.image.load(path.join(img_dir, 'bala.png')).convert()
+    assets["back_anim"] = pygame.image.load(path.join(img_dir, 'imagem 1.jpeg')).convert()
+    
     assets["score_font"] = pygame.font.Font(path.join(fnt_dir, "PressStart2P.ttf"), 28)
 
+    assets["musica_fim"] = pygame.mixer.Sound(path.join(snd_dir, 'Game Over Sound Effects High Quality-[AudioTrimmer.com].ogg'))
+
     
+    back_anim = []
+    for i in range (2):
+        filename = 'imagem {}.jpeg'.format(i)
+        img1 = pygame.image.load(path.join(img_dir,filename)).convert()
+        img1 = pygame.transform.scale(img1, (1000, 700))
+        back_anim.append(img1)
+    assets["back_anim"]=back_anim
         
     boneco_anim = []
     for i in range(5):
